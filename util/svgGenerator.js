e508004c2fb4c235d523b1469c670206e354b8e9
Roesn <Bubwub04@icloud.com>
2023-10-28 07:22:05 -0400

    cleaning

--- util/svgGenerator.js
+++ util/svgGenerator.js
@@ -11,28 +11,21 @@ function svgGenerator(input) {
     if (shape == "Circle") {
 
         let cir = new Circle();
-        // return string of svg file code for triangle
-        console.log("circle");
-        return `<svg width="300" height="200">
-    <circle cx="150" cy="100" r="100" fill=${shapeColor} />
-    <text x="50%" y="55%" dominant-baseline="middle" font-size=5rem text-anchor="middle" fill=${textColor}>${text}</text>
-</svg>`;
+
+        // return string of svg file code for circle
+        return `<svg width="300" height="200"></svg>`;
     } else if (shape == "Square") {
 
         let squ = new Square();
-        console.log("square");
-        return `<svg width="300" height="200">
-    <rect width="200" height="200" fill=${shapeColor} />
-    <text x="35%" y="50%" dominant-baseline="middle" font-size=5rem text-anchor="middle" fill=${textColor}>${text}</text>
-</svg>`;
+
+        // return string of svg file code for square
+        return `<svg width="300" height="200"></svg>`;
     } else {
 
         let tri = new Triangle();
-        console.log("triangle");
-        return `<svg width="300" height="200">
-    <polygon points="150,0 0,300 300,300" fill=${shapeColor} />
-    <text x="50%" y="65%" dominant-baseline="middle" font-size=3.5rem text-anchor="middle" fill=${textColor}>${text}</text>
-</svg>`;
+
+        // return string of svg file code for triangle
+        return `<svg width="300" height="200"></svg>`;
     }
 }
 
