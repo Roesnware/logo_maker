4378fcdd2a46b3ab8fdec51f3db4d51439fb5e33
Roesn <Bubwub04@icloud.com>
2023-10-28 05:19:29 -0400

    comments

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// require packages
 const inq = require('inquirer');
 const fs = require('fs');
 
+// use inquirer
 inq
+    // prompt for input 
     .prompt([
         {
 
@@ -10,10 +13,14 @@ inq
 
         },
     ])
+    // then use that data 
     .then((answers) => {
+        // var to store code for svg file 
         const svgCode = generateSVG(answers);
     
+        // write SVG file using answers 
         fs.writeFile(`${answers.text}.html`, svgCode, (err) => {
+            // if error log error else log success
           err ? console.log(err) : console.log('Successfully created SVG file');
         }
         );    
